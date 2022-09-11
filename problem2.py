262271d9641ec672b40bf442ff6411f9415d96c9
MD Shahadat Hossain Bhuian <103915287+Coder-Shahadat@users.noreply.github.com>
2022-09-11 23:52:44 +0600

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -4,7 +4,6 @@ def bubble(arr):
         for j in range(i+1,len(arr)):
             if arr[i]>arr[j]:
                 arr[i],arr[j]=arr[j],arr[i]
-                print(arr[i],arr[j])
     return arr
 
 
