ab2122f511e9f4cdb2e712bc53820a73146bcdc0
MD Shahadat Hossain Bhuian <103915287+Coder-Shahadat@users.noreply.github.com>
2022-09-12 08:01:37 +0600

    Update problem5.py

--- problem5.py
+++ problem5.py
@@ -1,39 +1,45 @@
-# Author: MD.Shahdat Hossain Bhuian
-def merge(array, left_index, right_index, middle):
-    left_copy = array[left_index:middle + 1]
-    right_copy = array[middle+1:right_index+1]
-    left_copy_index = 0
-    right_copy_index = 0
-    sorted_index = left_index
-
-    while left_copy_index < len(left_copy) and right_copy_index < len(right_copy):
-        if left_copy[left_copy_index] <= right_copy[right_copy_index]:
-            array[sorted_index] = left_copy[left_copy_index]
-            left_copy_index = left_copy_index + 1
-        else:
-            array[sorted_index] = right_copy[right_copy_index]
-            right_copy_index = right_copy_index + 1
-        sorted_index = sorted_index + 1
-
-    while left_copy_index < len(left_copy):
-        array[sorted_index] = left_copy[left_copy_index]
-        left_copy_index = left_copy_index + 1
-        sorted_index = sorted_index + 1
-
-    while right_copy_index < len(right_copy):
-        array[sorted_index] = right_copy[right_copy_index]
-        right_copy_index = right_copy_index + 1
-        sorted_index = sorted_index + 1
-
-
-def merge_sort(array, left_index, right_index):
-    if left_index >= right_index:
-        return
-    middle = (left_index + right_index)//2
-    merge_sort(array, left_index, middle)
-    merge_sort(array, middle + 1, right_index)
-    merge(array, left_index, right_index, middle)
+def merge(arr,start,mid,end):
+    merged=[0]*(end-start+1)
+    idx1=start
+    idx2=mid+1
+    x=0
     
-array = list(map(int,input("Enter an array: ").split()))
-merge_sort(array, 0, len(array) -1)
-print(array)
+    while idx1<=mid and idx2<=end:
+        if arr[idx1]<=arr[idx2]:
+            merged[x]=arr[idx1]
+            x+=1
+            idx1+=1
+        else:
+            merged[x]=arr[idx2]
+            x+=1
+            idx2+=1
+            
+    while idx1<=mid:
+        merged[x]=arr[idx1]
+        x+=1
+        idx1+=1
+    
+    while idx2<=end:
+        merged[x]=arr[idx2]
+        x+=1
+        idx2+=1
+    
+    j=start
+    for i in range(len(merged)):
+        arr[j]=merged[i]
+        j+=1 
+    
+    
+def merge_sort(arr,start,end):
+    if start<end:
+        mid = start+(end-start)//2
+        merge_sort(arr,start,mid)
+        merge_sort(arr,mid+1,end)
+        merge(arr,start,mid,end)
+        
+            
+arr=list(map(int,input("Enter the array: ").split()))
+print("Entered Array: ",arr)
+#function call
+merge_sort(arr,0,len(arr)-1)
+print("Sorted array: ",arr)
