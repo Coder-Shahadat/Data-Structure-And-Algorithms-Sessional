ce98c5ec8cb6029b42fce1068dab81096244faac
MD Shahadat Hossain Bhuian <103915287+Coder-Shahadat@users.noreply.github.com>
2022-09-12 08:35:45 +0600

    Update problem7.py

--- problem7.py
+++ problem7.py
@@ -1,19 +1,22 @@
-# Author: MD.Shahdat Hossain Bhuian
-
-def search(pat, txt):
-	M = len(pat)
-	N = len(txt)
-
-	for i in range(N - M + 1):
-		j = 0
-		while(j < M):
-			if (txt[i + j] != pat[j]):
-				break
-			j += 1
-		if (j == M):
-			print("Pattern found at index ", i)
-
-
-text=input("Enter a line of text: ")
-pattern=input("Enter a Pattern: ")
-search(pattern,text)
+def pattern_matching(pat,txt):
+    pl=len(pat)
+    tl=len(txt)
+    c=True
+    
+    for i in range(tl-pl+1):
+        j=0
+        while j<pl:
+            if txt[i+j]!=pat[j]:
+                break
+            j+=1
+        if j==pl:
+            print("Pattern Found in: ",i," index")    
+            c=False
+    if c:
+        print("Pattern Not Found")
+    
+    
+str=input("Enter String: ")
+pat=input("Enter Pattern: ")
+pattern_matching(pat,str)  
+            
